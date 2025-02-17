7b6efe4d177695c954e32eda7895cc25de482cc9
Yvir Cysera <62361212+YvirC@users.noreply.github.com>
2025-02-17 15:07:04 -0500

    Deleted Superfluous Stuff

--- Lab1.m
+++ Lab1.m
@@ -1,43 +1,21 @@
-%Lab 1 Matlab Code
+% Lab 1 Matlab Code
 % Inputting Variables (X,Y,Z) and Weight
 % Solving for Z'
 % Using Adjusted Z' to solve for Tension
-% Using Tension Equations to solve for Smalley-Pulley
+% Using Tension Equations to solve for Small-Pulley
 clear all
 close all
 clc
 
-% Function set -- Only usable for 3D
-function newVector = vectorMake(start, finish)
-   newVector = finish - start;
-end
-
-function length = vectorLength(vector)
-   length = sqrt((vector(1,1)^2) + (vector(1,2)^2) + (vector(1,3)^2));
-end
-
-function unitVector = normalize(vector)
-   unitVector = vector ./ vectorLength(vector);
-end
-
-function forceComponent = findComponent(vector, mag)
-   forceComponent = normalize(vector) .* mag;
-end
-
-function moment = findMoment(distanceVector, force)
-   moment = cross(distanceVector, force);
-end
-% Function set -- FINISH
-
-%Variables (meters) -- DO NOT TOUCH
+%Variables (meters) 
 pulley1 = [-2.42824 1.62306 2.965];
 pulley2 = [-2.41808 -1.72466 2.964];
 pulley3 = [2.40284 -0.18034 2.962];
 pulleySheaveRadius = 0.01905; % Corrected to metric (meters)
 pulleyAxleRadius = 0.0047625; % also in meters
 knotWeight = 1.372*9.8; % Newtons
-%Variables end -- DO NOT TOUCH
-disp("copy and paste a line [x,y,z] and put into prompted location")
+
+disp("Copy and paste a line [x,y,z] and put into prompted location")
 disp("Trial 1 Knot Coord [-1.1049,-0.32512,1.74498]")
 disp("Trial 2 Knot Coord [-1.38176,-0.58674,2.06756]")
 disp("Trial 3 Knot Coord [-1.39192,-0.74422,2.15138]")
@@ -57,40 +35,49 @@ pulley1 = pulley1 - knot;
 pulley2 = pulley2 - knot;
 pulley3 = pulley3 - knot;
 
-
 %ANGLE CALCUALTIONS
 %theta  = alpha + beta
 %Pulley 1
 z1 = pulley1(3);
+
 %Distance from knot to the pully on the x and y axis
 a1 = sqrt(((pulley1(1)^2) + (pulley1(2)^2)));
 alpha1 = atand(z1/(a1-pulleySheaveRadius));
+
 %beta
 pulleyCenter1 = pulley1(3)/sind(alpha1);
 beta1 = asind(pulleySheaveRadius/pulleyCenter1);
 theta1 = alpha1 +beta1;
+
 %Pulley 2
 z2 = pulley2(3);
+
 %Distance from knot to the pully on the x and y axis
 a2 = sqrt(((pulley2(1)^2) + (pulley2(2)^2)));
 alpha2 = atand(z2/(a2-pulleySheaveRadius));
+
 %beta
 pullCenter2 = pulley2(3)/sind(alpha2);
 beta2 = asind(pulleySheaveRadius/pullCenter2);
 theta2 = alpha2 +beta2;
+
 %Pulley 3
 z3 = pulley3(3);
+
 %Distance from knot to the pully on the x and y axis
 a3 = sqrt(((pulley3(1)^2) + (pulley3(2)^2)));
 alpha3 = atand(z3/(a3-pulleySheaveRadius));
+
 %beta
 pullCenter3 = pulley3(3)/sind(alpha3);
 beta3 = asind(pulleySheaveRadius/pullCenter3);
-theta3 = alpha3 +beta3;
+theta3 = alpha3 + beta3;
+
 %Recalculated z coord
 pulley1(3) = a1 * tand(theta1);
 pulley2(3) = a2 * tand(theta2);
 pulley3(3) = a3 * tand(theta3);
+
 %Lambda
 Lambda1 = pulley1./norm(pulley1);
 Lambda2 = pulley2./norm(pulley2);
@@ -99,24 +86,15 @@ Lambda3 = pulley3./norm(pulley3);
 Matrix = [Lambda1(1) Lambda2(1) Lambda3(1);Lambda1(2) Lambda2(2) Lambda3(2);Lambda1(3) Lambda2(3) Lambda3(3)];
 Sum = [0; 0; knotWeight];
 Result = Matrix\Sum;
-%Result2 = Matrix\Sum; %renames as new variable so as to not overwrite
-%Result2 = Result*(3.3291*9.8)
 
 %% FRICTION COEFFICENT CALCULATION %%
 
-
-TensionArry =  Result.'; %Turns Column into row
-%WeightArry = 9.8.*[0.9342, 1.1983, 1.1966; 0.9342, 1.466, 1.1966; 0.7865, 1.466, 1.1966]; %gives collected weights in N
-%WeightArry = 9.8.*[0.9342, 1.1983, 1.1966]; %gives collected weights in N
-
-
+TensionArry = Result.'; %Turns Column into row
 
 friction = abs(TensionArry-WeightArry)*(pulleySheaveRadius/pulleyAxleRadius);
 coeff = friction./sqrt(abs(TensionArry + WeightArry).^2 - friction.^2);
 
-
 %SMALL PULLEY ESTIMATION USING ORGINAL Z VALUES
-
 
 %Lambda
 
