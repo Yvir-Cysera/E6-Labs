e5c34da79f0c94c6d88beb1f1994ab0dd93e0616
Awhaven <Havenawenzel@gmail.com>
2025-03-03 22:50:36 -0500

    Lab2.m

--- Lab2.m
+++ Lab2.m
@@ -1,59 +1,194 @@
-% 2/21/25
-% ENGR006
-% Lab 2 Matlab Code
-% Steps:
-% Inputting Variables (X,Y,Z) and Weight
+%Lab 2: Devon, Haven, Alexis, Gael, Osmar, and Iver
 
-% clean workspace, figures and command window
-clear all
+clear 
 close all
 clc
 
-% Variables (meters) 
-pulley2 = [-2.47015 2.962];
-pulley4 = [2.39776 2.965];
-knotWeight = 1.372 * 9.8; % Newtons
+%% DATA
+% Pul2 = [-2.42824, 2.962]; %pulley location 2, [X Z] meters
+% Pul4 = [2.41681, 2.965]; 
+% 
+% HW = 9.8 * [1.372]; %Laser weight, N   
+% 
+% CWL1 = 0.0254 * [-25.2, 36.7]; %Laser location 1, [X Z] meters
+% CWL2 = 0.0254 * [-23.2, 29.7];
+% CWL3 = 0.0254 * [-15.1, 23.7];
+% 
+% CW1 = 9.8 * [1.0993, .8653]; %Counterweight by trial [pulley 2, pulley 4], newtons
+% CW2 = 9.8 * [1.0706, .8653];
+% CW3 = 9.8 * [.9754, .8653];
 
-% We display these coordinates from our experiment for simplicity's sake
-% Feel free to use your own dataset! :D
-%disp("Copy and paste a line [x,y,z] and put into prompted location")
-%disp("Trial 1 Knot Coord [-0.64008,1.25476]")
-%disp("Trial 2 Knot Coord [-0.58928,1.07696]")
-%disp("Trial 3 Knot Coord [-0.38354,0.92456]")
 
-% input
-%prompt = "Give a point for the knot: ";
-%prompt2 = "Give the counterweights: ";
-%knot = input(prompt);
-knot = [-0.64008,1.25476];
-disp(" ")
-%disp("Trial 1 counterweights in Newtons [10.784133 8.488593]")
-%disp("Trial 2 counterweights in Newtons [10.502586 8.488593]")
-%disp("Trial 3 counterweights in Newtons [9.568674 8.488593]")
-%WeightArry = input(prompt2);
-WeightArry = [10.784133 8.488593];
-disp(" ")
 
-% pulley coordinates with knot as origin
-vector2 = pulley2 - knot;
-vector4 = pulley4 - knot;
+%% TRIAL 1 
+% 0. Variable coordinates & weights [x, z] 
 
-% Distance from knot to the pulley on the x and z axis
-distance2 = sqrt(((vector2(1)^2))+(vector2(2)^2));
-distance4 = sqrt(((vector4(1)^2))+(vector4(2)^2));
-% y axis ignored since we only have 2 dimensions
-
-% find unit vector
-lambda2 = vector2 ./ distance2;
-lambda4 = vector4 ./ distance4;
-
-% fsolve
-%function F = myfun(x)
-
-%F(1) = (x(1).*lambda2(1)) + (x(2).*lambda4(1));
-%F(2) = (x(1).*lambda2(3)) + (x(2).*lambda4(3));
-%F(3) = (x(3).*x(1)) - (cross(vector4,x(3)));
-
-fun = @;
-x0 = [0,0,0];
+fun = @root2d;
+x0 = [.01905, 10.77, 8.4699]; %estimate based on counterweights and given value for radius
 x = fsolve(fun,x0);
+
+function F = root2d(x) %starts fSolve equation
+
+Pul2 = [-2.42824, 2.962]; %pulley location 2, [X Z] meters
+Pul4 = [2.41681, 2.965]; 
+
+HW = 9.8 * [1.372]; %Laser weight, N   
+
+CWL1 = 0.0254 * [-25.2, 36.7]; %Laser location 1, [X Z] meters
+
+CW1 = 9.8 * [1.0993, .8653]; %Counterweight by trial [pulley 2, pulley 4],newtons 
+
+
+%% 1. Calculation Shortcuts
+
+x2 = Pul2(1) - CWL1(1); %distance from centerweight to pulley in x and z
+x4 = Pul4(1) - CWL1(1);
+
+z2 = Pul2(2) - CWL1(2);
+z4 = Pul4(2) - CWL1(2);
+
+%% 2. Z' calulations
+a2 = atand( x2 / (x2-x(1)) ); %solves for pulley 2 alpha/beta/z' based z' equation
+b2 = asind(x(1)/sqrt((z4 - x(1))^2 + (z4)^2));
+zprime2 = atand(a2 + b2)
+m2 = sqrt(x2^2+z2^2); %magnitude based on these distances
+
+a4 = atand(z4/(x4-x(1))); %solves for pulley 4
+b4 = asind(x(1) / sqrt((z4 - x(1))^2 + z4^2));
+zprime4 = atand(a4 + b4)
+m4 = sqrt(x4^2+z4^2);
+
+%% 3 Equations in terms of radius
+
+%Fx=0 equation
+F(1) = (x(2) * x2)/m2 - (x(3) * z4)/m4;
+
+%Fz=0 equation
+F(2) = (x(2)*zprime2)/m2 + (x(3)*zprime4)/m4 - HW;
+
+%moment equation
+F(3) = ((x2 - x(1)) * zprime2)/m2 - (x2*z2)/m2 - x(1);
+
+%Trials 2 and 3 repeat the same process of trial 1.
+
+end 
+
+
+%% TRIAL 2 
+% 0. Variable coordinates & weights [x, z] 
+
+fun = @root2d2;
+y0 = [.01905, 10.49188, 8.47994]; %estimate based on counterweights and given value for radius
+y = fsolve(fun,y0);
+
+function A = root2d2(y)
+
+Pul2 = [-2.42824, 2.962]; %pulley location 2, [X Z] meters
+Pul4 = [2.41681, 2.965]; 
+
+HW = 9.8 * [1.372]; %Laser weight, N   
+
+CWL2 = 0.0254 * [-23.2, 29.7];
+
+%CW2 = 9.8 * [1.0706, .8653]; %gives basis for estimates
+
+%% 1. Calculation Shortcuts
+
+x2 = Pul2(1) - CWL2(1); %distance from centerweight to pulley in x and z
+x4 = Pul4(1) - CWL2(1);
+
+z2 = Pul2(2) - CWL2(2);
+z4 = Pul4(2) - CWL2(2);
+
+%% 2. Z' calulations
+a2 = atand( x2 / (x2-y(1)) ); %solves for pulley 2 alpha/beta/z' based z' equation
+b2 = asind(y(1)/sqrt((z4 - y(1))^2 + (z4)^2));
+zprime2 = atand(a2 + b2);
+m2 = sqrt(x2^2+z2^2);
+
+a4 = atand(z4/(x4-y(1))); %solves for pulley 4
+b4 = asind(y(1) / sqrt((z4 - y(1))^2 + z4^2));
+zprime4 = atand(a4 + b4);
+m4 = sqrt(x4^2+z4^2);
+
+%% 3 Equations in terms of radius
+
+%Fx=0 equation
+A(1) = (y(1) * x2)/m2 - (y(2) * z4)/m4;
+
+%Fz=0 equation
+A(2) = (y(1)*zprime2)/m2 + (y(2)*zprime4)/m4 - HW;
+
+%moment equation
+A(3) = ((x2 - y(1)) * zprime2)/m2 - (x2*z2)/m2 - y(1);
+
+end 
+
+%% TRIAL 3 
+% 0. Variable coordinates & weights [x, z] 
+
+fun = @root2d3;
+z0 = [.01905, 9.55892, 8.47994]; %estimate based on counterweights and given value for radius
+z = fsolve(fun,z0);
+
+function B = root2d3(z)
+
+Pul2 = [-2.42824, 2.962]; %pulley location 2, [X Z] meters
+Pul4 = [2.41681, 2.965]; 
+
+HW = 9.8 * [1.372]; %Laser weight, N   
+
+CWL3 = 0.0254 * [-15.1, 23.7];
+
+% CW3 = 9.8 * [.9754, .8653];
+
+%% 1. Calculation Shortcuts
+
+x2 = Pul2(1) - CWL3(1); %distance from centerweight to pulley in x and z
+x4 = Pul4(1) - CWL3(1);
+
+z2 = Pul2(2) - CWL3(2);
+z4 = Pul4(2) - CWL3(2);
+
+%% 2. Z' calulations
+a2 = atand( x2 / (x2-z(1)) ); %solves for pulley 2 alpha/beta/z' based z' equation
+b2 = asind(z(1)/sqrt((z4 - z(1))^2 + (z4)^2));
+zprime2 = atand(a2 + b2);
+m2 = sqrt(x2^2+z2^2);
+
+a4 = atand(z4/(x4-z(1))); %solves for pulley 4
+b4 = asind(z(1) / sqrt((z4 - z(1))^2 + z4^2));
+zprime4 = atand(a4 + b4);
+m4 = sqrt(x4^2+z4^2);
+
+%% 3 Equations in terms of radius
+
+%Fx=0 equation
+B(1) = (z(2) * x2)/m2 - (z(3) * z4)/m4;
+
+%Fz=0 equation
+B(2) = (z(2)*zprime2)/m2 + (z(3)*zprime4)/m4 - HW;
+
+%moment equation
+B(3) = abs(((x2 - z(1)) * zprime2)/m2 - (x2*z2)/m2) - z(1);
+
+end 
+
+
+%% 5. STANDARD DEVIATION CALCULATION
+clc
+
+disp('Our three estimated radii values are below')
+Radii = [x(1), y(1), z(1)]
+disp('Estimated tension value for pulley 2 from Trial 1, 2, and 3')
+Tension_2 = [x(2), y(2), z(2)]
+disp('Estimated tension value for pulley 4 from Trial 1, 2, and 3')
+Tension_4 = [x(3), y(3), z(3)]
+
+Average = mean(Radii) %Standard Deviation calculation
+StanDev = ((sum((Radii-Average).^2))./(3))^.5;
+StanDevRange = [Average - 3*StanDev, Average + 3*StanDev];
+
+formatSpec = ['The Standard Deviation is %f2 meters. ' ... %displays standard deviation
+    'The 3 STD range of the estimated radius is %f4 meters to %f5 meters.'];
+fprintf(formatSpec,StanDev,StanDevRange(1),StanDevRange(2))
